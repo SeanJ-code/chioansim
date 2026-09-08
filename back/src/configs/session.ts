c9180b125f8fec7b7001a65baf4f10f73b68b0e8
sean <newqqa11@gmail.com>
2026-09-08 11:21:47 +0800

    修改session

--- back/src/configs/session.ts
+++ back/src/configs/session.ts
@@ -5,20 +5,25 @@ import mongoose from 'mongoose'
 import { getSessionSecret } from './env'
 
 export function createSessionMiddleware(): RequestHandler {
+  const isProduction = process.env.NODE_ENV === 'production'
+
   return session({
     name: process.env.SESSION_COOKIE_NAME || 'chioansim.sid',
     secret: getSessionSecret(),
+
     proxy: true,
     resave: false,
     saveUninitialized: false,
+
     store: MongoStore.create({
       client: mongoose.connection.getClient(),
       ttl: 60 * 60 * 24 * 7,
     }),
+
     cookie: {
       httpOnly: true,
+      secure: isProduction,
       sameSite: 'lax',
-      secure: (process.env.NODE_ENV ?? 'production') === 'production',
       path: '/',
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
